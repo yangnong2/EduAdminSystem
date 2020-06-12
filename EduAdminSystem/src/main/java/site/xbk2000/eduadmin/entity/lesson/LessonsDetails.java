32925bcc769df91eddc000965ad85987cedc6590
xbk2000 <823972629@qq.com>
2020-06-12 11:08:12 +0800

    添加课程明细表和对应实体类

---
+++ EduAdminSystem/src/main/java/site/xbk2000/eduadmin/entity/lesson/LessonsDetails.java
@@ -0,0 +1,11 @@
+package site.xbk2000.eduadmin.entity.lesson;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 课程明细表对应的实体类，存放每门课程在一周内的具体上课时间
+ * @author xbk
+ */
+@Component
+public class LessonsDetails {
+}
